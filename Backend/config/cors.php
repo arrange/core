7c34e15c54421fef30fe4d96ecbe77c3ae1c029e
bhaumikwork <bhaumikwork@gmail.com>
2015-12-25 15:16:43 +0530

    change in cors

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -2,9 +2,9 @@
 return [
 	'supportsCredentials' => false,
 	'allowedOrigins' => ['*'],
-	'allowedHeaders' => ['Content-Type', 'Accept','Token','X-Requested-With'],
-	//'allowedHeaders' => ['*'],
-	'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
+	//'allowedHeaders' => ['Content-Type', 'Accept','Token','X-Requested-With'],
+	'allowedHeaders' => ['*'],
+	'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE','OPTIONS'],
 	'exposedHeaders' => [],
 	'maxAge' => 0,
 	'hosts' => [],
