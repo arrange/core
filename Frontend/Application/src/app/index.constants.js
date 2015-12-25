3607da4170579bd48e4384bf15602475d10e6c92
bhaumikwork <bhaumikwork@gmail.com>
2015-12-25 18:34:38 +0530

    application dist 25-12-2015 06:34

--- Frontend/Application/src/app/index.constants.js
+++ Frontend/Application/src/app/index.constants.js
@@ -1,22 +1,24 @@
 /* global malarkey:false, toastr:false, moment:false */
 (function() {
     'use strict';
-    var api = "http://localhost/notrierepo/Backend/public/";
+   // var api = "http://localhost/notrierepo/Backend/public/";
+   // var api1 = "http://localhost/notrierepo/Backend/";
+    var api = "http://api.notrie.com/";
+    var api1 = "http://api1.notrie.com/";
     angular
         .module('easywebapp')
         .constant('toastr', toastr)
         .constant('moment', moment)
         .constant('$config',{
            // url : "http://localhost:3000/",
-          //  api :  api,
           //  preset_thumb_url :  "http://localhost/notrierepo/Backend/public/preset-thumb",
           //  project_thumb_url :  "http://localhost/notrierepo/Backend/public/preview/file",
           //  clients_path : "http://localhost/notrierepo/Backend/clients",
+          //   api : "http://api.notrie.com/",
+            api :  api,
             url : "http://notrie.com/",
-            api : "http://api.notrie.com/",
-            preset_thumb_url :  "http://api.notrie.com/preset-thumb",
-            project_thumb_url :  "http://api.notrie.com/preview/file",
-            clients_path : "http://api1.notrie.com/clients",
+            preset_thumb_url : api + "preset-thumb",
+            clients_path : api1 + "clients",
             module : {
                 auth : {
                     controller : 'app/auth/controllers/',
